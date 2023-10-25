08da12f25c73b24234bbfe0b6dc604d1360ed4fc
KamilS <70943410+StayTrue2115@users.noreply.github.com>
2023-10-25 19:17:12 +0200

    Add files via upload

---
+++ zadania-25-10-2023/zadanie1.php
@@ -0,0 +1,20 @@
+<?php
+setcookie("username", "Kamil Stańczyk");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    if(!isset($_COOKIE["username"])){
+        echo "Cookie is not set!";
+    }else{
+        echo "Cookie is set!";
+    }
+    ?>
+</body>
+</html>
